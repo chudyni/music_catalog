d07ca9a0e95cdbdb8f0bb6308b1009af1638be0c
Marcin Bracisiewicz <m.bracisiewicz@gmail.com>
2017-08-30 20:02:37 +0200

    Change to MySQL - DB on docker + app locally - easier both on docker

--- src/main/java/com/example/musiccatalog/data/TestDataProvider.java
+++ src/main/java/com/example/musiccatalog/data/TestDataProvider.java
@@ -2,12 +2,18 @@ package com.example.musiccatalog.data;
 
 import com.example.musiccatalog.domain.Album;
 import com.example.musiccatalog.domain.Band;
+import com.example.musiccatalog.domain.auth.Role;
+import com.example.musiccatalog.domain.auth.User;
 import com.example.musiccatalog.repository.AlbumRepository;
 import com.example.musiccatalog.repository.BandRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+
 
 /**
  * Created by marcin.bracisiewicz
@@ -23,6 +29,22 @@ public class TestDataProvider {
 
     @PostConstruct
     public void init() {
+//        this.loadUsersRoles();
+        this.loadBandsAlbums();
+    }
+
+//    private void loadUsersRoles() {
+//        final Role user = new Role("USER_ROLE");
+//        final Role admin = new Role ("ADMIN_ROLE");
+//
+//        final User marcinBracisiewicz = new User("m.bracisiewicz@gmail.com", "hello");
+//        marcinBracisiewicz.setRoles(new HashSet<>(Arrays.asList(admin, user)));
+//
+//        final User bugReporter = new User("bugreporter@gmail.com", "bug");
+//        bugReporter.setRoles(Collections.singleton(user));
+//    }
+
+    private void loadBandsAlbums() {
         final Band aliceInChains = new Band("Alice In Chains");
         this.bandRepository.save(aliceInChains);
 
